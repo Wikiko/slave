c276ae25eb100ebb4a98c4fc3f36c7377a410676
William Azevedo <willianray@gmail.com>
2020-09-11 17:19:42 -0300

    Começando a melhorar

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,93 @@
-use std::process::Command;
-use std::io::{stderr, Write, BufReader, BufRead};
 use std::fs::File;
 use std::io;
+use std::io::{BufRead, BufReader};
 
-fn main() {
-    let git_command = Command::new("git")
-        .arg("checkout")
-        .arg("-b")
-        .arg("release/1.0.2")
-        .output()
-        .expect("Git failed to execute command");
+use structopt::StructOpt;
 
-    if !git_command.status.success() {
-        println!("Não foi possível trocar de branch");
-        stderr().write_all(&git_command.stderr).expect("Falha ao reportar o resultado do comando git");
-        return;
+#[derive(StructOpt, Debug)]
+enum ReleaseTypes {
+    MAJOR,
+    MINOR,
+    PATCH,
+}
+
+#[derive(StructOpt)]
+struct Cli {
+    #[structopt(subcommand)]
+    release_type: ReleaseTypes,
+}
+
+struct AndroidVersionUpgrader {
+    file: File,
+    current_version_code: u8,
+    current_version_name: String,
+}
+
+impl AndroidVersionUpgrader {
+    pub fn new() -> Result<AndroidVersionUpgrader, Box<dyn std::error::Error>> {
+        let file = File::open("./build.gradle")?;
+        let reader = BufReader::new(&file);
+        let mut version_name = String::new();
+        let mut version_code: u8 = 0;
+
+        for line in reader.lines() {
+            let line = line?;
+            if line.contains("versionName") {
+                version_name = String::from(line
+                    .replace("\"", "")
+                    .split_whitespace()
+                    .last()
+                    .expect("version name não encontrado"));
+            }
+            if line.contains("versionCode ") {
+                version_code = line
+                    .split_whitespace()
+                    .last()
+                    .expect("version code não encontrado")
+                    .parse()
+                    .expect("falha ao converter version code");
+            }
+        }
+
+        Ok(AndroidVersionUpgrader {
+            file,
+            current_version_code: version_code,
+            current_version_name: version_name,
+        })
     }
-    stderr().write_all(&git_command.stderr).expect("Falha ao reportar o resultado do comando git");
 
-    increment_version_of_gradle().expect("Ocorreu um erro ao trocar a versão no gradle");
+    fn get_next_version_code(&self) -> u8 {
+        self.current_version_code + 1
+    }
 
+    fn get_next_version_name(&self, release_type: ReleaseTypes) -> Result<String, Box<dyn std::error::Error>> {
+        let splited_version_name = self.current_version_name.split(".");
+        match release_type {
+            ReleaseTypes::MAJOR => {
+                Ok(String::new())
+            }
+            ReleaseTypes::MINOR => {
+                Ok(String::new())
+            }
+            ReleaseTypes::PATCH => {
+                let last = splited_version_name.last().unwrap();
+                let last: u8 = last.parse()?;
+                let last = last + 1;
+                println!("{}", last);
+                Ok(String::new())
+            }
+        }
+    }
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let cli: Cli = Cli::from_args();
+    let android_version_upgrader = AndroidVersionUpgrader::new()?;
+    println!("version_code: {}", android_version_upgrader.current_version_code);
+    println!("version_name: {}", android_version_upgrader.current_version_name);
+    println!("nextVersioName: {:?}", android_version_upgrader.get_next_version_name(cli.release_type)?);
+
+    Ok(())
 }
 
 fn increment_version_of_gradle() -> Result<(), io::Error> {
@@ -29,7 +97,10 @@ fn increment_version_of_gradle() -> Result<(), io::Error> {
     for line in reader.lines() {
         let line = line?;
         if line.contains("versionName") {
-            let version_name = line.split_whitespace().last().expect("version name não encontrado");
+            let version_name = line
+                .split_whitespace()
+                .last()
+                .expect("version name não encontrado");
             let version_name = version_name.replace("\"", "");
             let test = version_name.split(".");
             if let Some(last_version_number) = test.last() {
