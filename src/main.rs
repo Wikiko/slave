104f427ed86bb557d0b11a2ef8dfeae990578b49
William Azevedo <willianray@gmail.com>
2020-09-14 18:26:40 -0300

    Alterações para melhoria do código.

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,15 @@
 use std::collections::HashMap;
-use std::fs::{File, rename, remove_file};
+use std::fs::{File, remove_file, rename};
 use std::io::{BufRead, BufReader, LineWriter, Write};
+use std::process::{Command, exit};
 
 use structopt::StructOpt;
 
 use ReleaseTypes::*;
-use std::process::Command;
+use std::fmt::{Display, Formatter};
+use std::fmt;
 
-#[derive(StructOpt, Debug, PartialEq, Eq, Hash)]
+#[derive(StructOpt, Debug, PartialEq, Eq, Hash, Clone)]
 enum ReleaseTypes {
     MAJOR,
     MINOR,
@@ -20,29 +22,142 @@ struct Cli {
     release_type: ReleaseTypes,
 }
 
+
+struct SemanticVersion {
+    current: HashMap<ReleaseTypes, u8>,
+}
+
+impl Clone for SemanticVersion {
+    fn clone(&self) -> Self {
+        let mut map = HashMap::new();
+        map.clone_from(&self.current);
+        SemanticVersion {
+            current: map,
+        }
+    }
+
+    fn clone_from(&mut self, source: &Self) {
+        self.current = source.current.clone();
+    }
+}
+
+impl From<SemanticVersion> for String {
+    fn from(version: SemanticVersion) -> Self {
+        version.to_string()
+    }
+}
+
+impl Display for SemanticVersion {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self.to_string())
+    }
+}
+
+impl SemanticVersion {
+    fn from_string(version_string: &String) -> SemanticVersion {
+        let mut map: HashMap<ReleaseTypes, u8> = HashMap::new();
+        let vec_version_string: Vec<&str> = version_string
+            .split(".")
+            .collect();
+
+        map.insert(
+            MAJOR, vec_version_string
+                .get(0)
+                .expect("Não foi possível pegar a MAJOR")
+                .parse()
+                .expect("Não foi possível pegar a MAJOR"),
+        );
+        map.insert(
+            MINOR, vec_version_string
+                .get(1)
+                .expect("Não foi possível pegar a MAJOR")
+                .parse()
+                .expect("Não foi possível pegar a MAJOR"),
+        );
+        map.insert(
+            PATCH, vec_version_string
+                .get(2)
+                .expect("Não foi possível pegar a MAJOR")
+                .parse()
+                .expect("Não foi possível pegar a MAJOR"),
+        );
+
+        SemanticVersion {
+            current: map
+        }
+    }
+
+    fn format_version_name(&self, vec: &[String; 3]) -> String {
+        vec.join(".")
+    }
+
+    fn to_string(&self) -> String {
+        let version_name_vec = [
+            self.current[&MAJOR].to_string(),
+            self.current[&MINOR].to_string(),
+            self.current[&PATCH].to_string()
+        ];
+        self.format_version_name(&version_name_vec)
+    }
+
+    fn next_version(&self, release_type: &ReleaseTypes) -> SemanticVersion {
+        match release_type {
+            MAJOR => {
+                let mut major = self.current[&MAJOR];
+                major += 1;
+                let new_version_name = [
+                    major.to_string(),
+                    String::from("0"),
+                    String::from("0")
+                ];
+
+                SemanticVersion::from_string(
+                    &self.format_version_name(&new_version_name)
+                )
+            }
+            MINOR => {
+                let mut minor = self.current[&MINOR];
+                minor += 1;
+                let new_version_name = [
+                    self.current[&MAJOR].to_string(),
+                    minor.to_string(),
+                    String::from("0")
+                ];
+                SemanticVersion::from_string(
+                    &self.format_version_name(&new_version_name)
+                )
+            }
+            PATCH => {
+                let mut patch = self.current[&PATCH];
+                patch += 1;
+                let new_version_name = [
+                    self.current[&MAJOR].to_string(),
+                    self.current[&MINOR].to_string(),
+                    patch.to_string()
+                ];
+                SemanticVersion::from_string(
+                    &self.format_version_name(&new_version_name)
+                )
+            }
+        }
+    }
+}
+
 struct AndroidVersionUpgrader {
     file_path: String,
     current_version_code: u8,
-    current_version_name: HashMap<ReleaseTypes, u8>,
+    current_version: SemanticVersion,
 }
 
 impl AndroidVersionUpgrader {
-    pub fn new() -> Result<AndroidVersionUpgrader, Box<dyn std::error::Error>> {
+    pub fn new(current_version: SemanticVersion) -> Result<AndroidVersionUpgrader, Box<dyn std::error::Error>> {
         let file_path = String::from("./build.gradle");
         let file = File::open(&file_path)?;
         let reader = BufReader::new(&file);
-        let mut version_name = String::new();
         let mut version_code: u8 = 0;
 
         for line in reader.lines() {
             let line = line?;
-            if line.contains("versionName") {
-                version_name = String::from(line
-                    .replace("\"", "")
-                    .split_whitespace()
-                    .last()
-                    .expect("version name não encontrado"));
-            }
             if line.contains("versionCode ") {
                 version_code = line
                     .split_whitespace()
@@ -53,24 +168,10 @@ impl AndroidVersionUpgrader {
             }
         }
 
-        let mut map: HashMap<ReleaseTypes, u8> = HashMap::new();
-        let splited_version_name: Vec<&str> = version_name.split(".").collect();
-        map.insert(MAJOR, splited_version_name[0]
-            .parse()
-            .expect("Não foi possível pegar a versão MAJOR"));
-        map.insert(MINOR, splited_version_name[1]
-            .parse()
-            .expect("Não foi possível pegar a versão MINOR"));
-        map.insert(PATCH, splited_version_name[2]
-            .parse()
-            .expect("Não foi possível pegar a versão PATCH"));
-
-        let map = map;
-
         Ok(AndroidVersionUpgrader {
             file_path,
             current_version_code: version_code,
-            current_version_name: map,
+            current_version,
         })
     }
 
@@ -79,54 +180,14 @@ impl AndroidVersionUpgrader {
     }
 
     fn get_current_version_name(&self) -> String {
-        let version_name_vec = [
-            self.current_version_name[&MAJOR].to_string(),
-            self.current_version_name[&MINOR].to_string(),
-            self.current_version_name[&PATCH].to_string()
-        ];
-        self.format_version_name(&version_name_vec)
+        self.current_version.to_string()
     }
 
     fn get_next_version_name(&self, release_type: &ReleaseTypes) -> String {
-        match release_type {
-            MAJOR => {
-                let mut major = self.current_version_name[&MAJOR];
-                major += 1;
-                let new_version_name = [
-                    major.to_string(),
-                    String::from("0"),
-                    String::from("0")
-                ];
-                self.format_version_name(&new_version_name)
-            }
-            MINOR => {
-                let mut minor = self.current_version_name[&MINOR];
-                minor += 1;
-                let new_version_name = [
-                    self.current_version_name[&MAJOR].to_string(),
-                    minor.to_string(),
-                    String::from("0")
-                ];
-                self.format_version_name(&new_version_name)
-            }
-            PATCH => {
-                let mut patch = self.current_version_name[&PATCH];
-                patch += 1;
-                let new_version_name = [
-                    self.current_version_name[&MAJOR].to_string(),
-                    self.current_version_name[&MINOR].to_string(),
-                    patch.to_string()
-                ];
-                self.format_version_name(&new_version_name)
-            }
-        }
+        self.current_version.next_version(release_type).to_string()
     }
 
-    fn format_version_name(&self, vec: &[String; 3]) -> String {
-        vec.join(".")
-    }
-
-    fn upgrade(&self, release_type: ReleaseTypes) -> Result<(), Box<dyn std::error::Error>> {
+    fn upgrade(&self, release_type: &ReleaseTypes) -> Result<(), Box<dyn std::error::Error>> {
         let new_file_path = format!("{}.new", &self.file_path);
         let file = File::open(&self.file_path)?;
         let new_file = File::create(&new_file_path)?;
@@ -147,9 +208,71 @@ impl AndroidVersionUpgrader {
                 let new_version_code = line
                     .replace(
                         &self.current_version_code.to_string(),
-                        &self.get_next_version_code().to_string()
+                        &self.get_next_version_code().to_string(),
                     );
                 to_write = new_version_code;
+            }
+            writeln!(writer, "{}", to_write)?;
+        }
+        writer.flush()?;
+
+        remove_file(&self.file_path)?;
+        rename(&new_file_path, &self.file_path)?;
+
+        Ok(())
+    }
+}
+
+struct PackageJsonUpgrader {
+    file_path: String,
+    current_version: SemanticVersion
+}
+
+impl PackageJsonUpgrader {
+    fn new() -> Result<PackageJsonUpgrader, Box<dyn std::error::Error>> {
+        let file_path = String::from("./package.json");
+        let file = File::open(&file_path)?;
+        let reader = BufReader::new(&file);
+        let mut version_string = String::new();
+
+        for line in reader.lines() {
+            let line = line?;
+            if line.contains("\"version\"") {
+                version_string = String::from(line
+                    .replace("\"", "")
+                    .replace(",", "")
+                    .split_whitespace()
+                    .last()
+                    .expect("version não encontrado"));
+            }
+        }
+
+        Ok(PackageJsonUpgrader {
+            file_path,
+            current_version: SemanticVersion::from_string(&version_string),
+        })
+    }
+
+    fn get_current_version(&self) -> String {
+        self.current_version.to_string()
+    }
+
+    fn upgrade(&self, release_type: &ReleaseTypes) -> Result<(), Box<dyn std::error::Error>> {
+        let new_file_path = format!("{}.new", &self.file_path);
+        let file = File::open(&self.file_path)?;
+        let new_file = File::create(&new_file_path)?;
+        let reader = BufReader::new(&file);
+        let mut writer = LineWriter::new(new_file);
+        for line in reader.lines() {
+            let line = line?;
+            let mut to_write = line.clone();
+            if line.contains("\"version\"") {
+                let new_version_name = line
+                    .replace(
+                        &self.current_version.to_string(),
+                        &self.current_version.next_version(release_type).to_string(),
+                    );
+                to_write = new_version_name;
             }
             writeln!(writer, "{}", to_write)?;
         }
@@ -165,17 +288,26 @@ impl AndroidVersionUpgrader {
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let cli: Cli = Cli::from_args();
 
-    let android_version_upgrader = AndroidVersionUpgrader::new()?;
+    let package_upgrader = PackageJsonUpgrader::new()?;
 
+    let next_version = &package_upgrader
+        .current_version.next_version(&cli.release_type);
 
-    Command::new("git")
-        .arg("branch")
-        .arg("-b")
-        .arg("master")
-        .arg(format!("release/{}", android_version_upgrader.get_next_version_name(&cli.release_type)))
-        .spawn()?;
+    let android_version_upgrader = AndroidVersionUpgrader::new(package_upgrader.current_version.clone())?;
 
-    android_version_upgrader.upgrade(cli.release_type)?;
+    // let git_checkout = Command::new("git")
+    //     .arg("checkout")
+    //     .arg("-b")
+    //     .arg(format!("release/{}", &next_version))
+    //     .arg("master")
+    //     .status()?;
+    //
+    // if !git_checkout.success() {
+    //     exit(git_checkout.code().unwrap());
+    // }
+
+    android_version_upgrader.upgrade(&cli.release_type)?;
+    package_upgrader.upgrade(&cli.release_type)?;
 
     Ok(())
 }
